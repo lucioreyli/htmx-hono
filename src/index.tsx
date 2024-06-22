58ce62364fa9996a33baece368f5c67292f18672
Lúcio Andrade <lucioandradejr@gmail.com>
2024-06-22 15:01:46 -0400

    feat: docker build

--- src/index.tsx
+++ src/index.tsx
@@ -7,34 +7,29 @@ const app = new Hono();
 let total = 0;
 
 app
-  .get("/", renderer, async (c) => {
-    const data = (
-      <div>
-        <style>
-          {raw`
-					.htmx-request {
+	.get("/", renderer, async (c) => {
+		const data = (
+			<div>
+				<style>
+					{raw`
+          .htmx-request {
 						pointer-events: none;
 					}
 					`}
-        </style>
-        <h1>Login</h1>
-        <span>a login page</span>
-        <button
-          type="button"
-          hx-post="/"
-          hx-target="#container"
-          hx-trigger="click:setAttribute(disabled)"
-        >
-          increment
-        </button>
-        <div id="container">total = {total}</div>
-      </div>
-    );
-    return c.render(data);
-  })
-  .post(async (c) => {
-    await new Promise((r) => setTimeout(r, 2000));
-    return c.html(`total = ${++total}`);
-  });
+				</style>
+				<h1>Login</h1>
+				<span>a login page</span>
+				<button type="button" hx-post="/" hx-target="#container">
+					increment
+				</button>
+				<div id="container">total = {total}</div>
+			</div>
+		);
+		return c.render(data);
+	})
+	.post((c) => {
+		// await new Promise((r) => setTimeout(r, 2000));
+		return c.html(`total = ${++total}`);
+	});
 
 export default app;
